4732ec1baff6fdc7f4f052d2186e6cb920e5a8ba
Crowdbotics <team@crowdbotics.com>
2020-09-25 06:11:42 +0000

    Update components

--- src/features/BlankScreen211144/screens/blank.js
+++ src/features/BlankScreen211144/screens/blank.js
@@ -23,7 +23,21 @@ export class __Blank extends React.Component {
   render = () => (
     <View style={this.props.themedStyle.View_1}>
       <View style={this.props.themedStyle.View_2}>
-        <View style={this.props.themedStyle.View_3} />
+        <View style={this.props.themedStyle.View_3}>
+          <Button
+            textStyle={{
+              fontSize: 12,
+              color: "#000000",
+              textAlign: "center",
+              fontWeight: "normal",
+              fontStyle: "normal"
+            }}
+            style={this.props.themedStyle.Button_202}
+            onPress={() => alert("Pressed!")}
+          >
+            Press me!
+          </Button>
+        </View>
         <View style={this.props.themedStyle.View_4}>
           <View style={this.props.themedStyle.View_12}>
             <View style={this.props.themedStyle.View_200} />
@@ -128,6 +142,37 @@ _Blank = withStyles(__Blank, theme => ({
     borderBottomWidth: 0,
     borderRadius: 0,
     backgroundSize: "auto"
+  },
+  Button_202: {
+    width: "100%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#3366FF",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
   },
   View_4: {
     marginLeft: 0,
